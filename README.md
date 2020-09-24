7b59edfdfcd76ccb1330cc46d9392500fa0b18a3
Stud. Nicolas Dovetta <n.dovetta.0823@vallauri.net>
2020-09-24 12:33:55 +0200

    First commit - date: 24/09/2020

---
+++ README.md
@@ -0,0 +1,3 @@
+# FORMULA ONE PROJECT
+## DOVETTA NICOLAS - 5^B Informatica A.S. 2020/2021
+### I.I.S. "G. Vallauri" - Fossano (CN)
---
+++ formulaOne_console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace formulaOne_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
