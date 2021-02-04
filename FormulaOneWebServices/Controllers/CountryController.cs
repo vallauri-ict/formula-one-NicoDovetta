3af83b321efa62b4c2974f9f008a59d1634e1100
Stud. Nicolas Dovetta <n.dovetta.0823@vallauri.net>
2021-02-04 13:09:07 +0100

    Progetto api proprietario formulaone

---
+++ FormulaOneWebServices/Controllers/CountryController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using dll_Utilities;
+using System.Data;
+
+namespace FormulaOneWebServices
+{
+    //api/Country
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private const string WORKINGPATH = @"C:\data\formulaOne\";
+        private const string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + WORKINGPATH + @"FormulaOne.mdf;Integrated Security=True";
+        Utilities_Database db = new Utilities_Database(WORKINGPATH, CONNECTION_STRING);
+
+        // GET: api/Country
+        [HttpGet]
+        public List<dll_Utilities.Models.Country> Get()
+        {
+            return db.getTableCountry();
+        }
+
+        // GET: api/Country/5
+        [HttpGet("{countryCode}", Name = "Get")]
+        public dll_Utilities.Models.Country Get(string id)
+        {
+            return db.getTableCountryByCode(id);
+        }
+
+        // POST: api/Country
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Country/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ dll_Utilities/Models/Country.cs
@@ -0,0 +1,14 @@
+﻿namespace dll_Utilities.Models
+{
+    public class Country
+    {
+        public Country(string countryCode, string countryName)
+        {
+            this.countryCode = countryCode;
+            this.countryName = countryName;
+        }
+
+        public string countryCode { get; set; }
+        public string countryName { get; set; }
+    }
+}
