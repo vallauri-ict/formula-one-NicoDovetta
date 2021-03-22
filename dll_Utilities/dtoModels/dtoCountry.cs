6582a8d5a6ced211d370a8c6953f20608c98084b
Stud. Nicolas Dovetta <n.dovetta.0823@vallauri.net>
2021-03-22 09:46:59 +0100

    File dei dto

---
+++ dll_Utilities/dtoModels/dtoCountry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace dll_Utilities.dtoModels
+{
+	public class dtoCountry
+	{
+        private string isoCode;
+        private string name;
+
+        public string IsoCode { get => isoCode; set => isoCode = value; }
+        public string Name { get => name; set => name = value; }
+
+
+        public dtoCountry(string isoCode, string name)
+        {
+            this.isoCode = isoCode;
+            this.name = name;
+        }
+
+        public dtoCountry(SqlDataReader reader)
+        {
+            this.IsoCode = reader.GetString(0);
+            this.Name = reader.GetString(1);
+        }
+    }
+}
---
+++ dll_Utilities/dtoModels/dtocircuit.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace dll_Utilities.dtoModels
+{
+	public class dtoCircuit
+	{
+        private int id;
+        private string countryCode;
+        private string place;
+        private string name;
+        private int laps;
+
+        public int Id { get => id; set => id = value; }
+        public string CountryCode { get => countryCode; set => countryCode = value; }
+        public string Place { get => place; set => place = value; }
+        public string Name { get => name; set => name = value; }
+        public int Laps { get => laps; set => laps = value; }
+
+        public dtoCircuit(int id, string countryCode, string place, string name, int laps)
+        {
+            this.Id = id;
+            this.CountryCode = countryCode;
+            this.Place = place;
+            this.Name = name;
+            this.Laps = laps;
+        }
+
+        public dtoCircuit(SqlDataReader reader)
+        {
+            this.Id = reader.GetInt32(0);
+            this.CountryCode = reader.GetString(1);
+            this.Place = reader.GetString(2);
+            this.Name = reader.GetString(3);
+            this.Laps = reader.GetInt32(4);
+        }
+    }
+}
