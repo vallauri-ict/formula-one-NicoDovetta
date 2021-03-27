c7e747732a641cf1e87a10061f5fbfce531bad8b
Dovetta Nicolas <nicodovetta@gmail.com>
2021-03-27 22:01:07 +0100

    One of the latest

---
+++ dll_Utilities/dtoModels/dtoRace.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using dll_Utilities.Models;
+
+namespace dll_Utilities.dtoModels
+{
+	public class dtoRace
+	{
+		private int raceId;
+		private int circuitId;
+		private DateTime date;
+
+		public int RaceId { get => raceId; set => raceId = value; }
+		public int CircuitId { get => circuitId; set => circuitId = value; }
+		public DateTime Date { get => date; set => date = value; }
+
+		public dtoRace(int raceId, int circuitId, DateTime date)
+		{
+			RaceId = raceId;
+			CircuitId = circuitId;
+			Date = date;
+		}
+
+		public dtoRace(SqlDataReader reader)
+		{
+			RaceId = reader.GetInt32(0);
+			CircuitId = reader.GetInt32(1);
+			date = reader.GetDateTime(2);
+		}
+	}
+}
